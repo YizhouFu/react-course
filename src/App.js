ddb1fc0acdc6d8389ebbd6f3627cedf3d83bef09
JadFu <yfu5@aceproject.space>
2023-09-06 08:25:50 -0500

    add wapper layout
    add wapper layout with props.children

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   //localhost:3000/
@@ -18,18 +18,19 @@ function App() {
     ),
     (
       <div>
-        <MainNavigation />
-        <Switch>
-          <Route path="/" exact={true}>
-            <AllMeetupsPage />
-          </Route>
-          <Route path="/new-meetups">
-            <NewMeetupsPage />
-          </Route>
-          <Route path="/favorites">
-            <FavoritesPage />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route path="/" exact={true}>
+              <AllMeetupsPage />
+            </Route>
+            <Route path="/new-meetups">
+              <NewMeetupsPage />
+            </Route>
+            <Route path="/favorites">
+              <FavoritesPage />
+            </Route>
+          </Switch>
+        </Layout>
       </div>
     )
   );
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -15,9 +15,9 @@ const DUMMY_DATA = [
     title: "This is a second meetup",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
+    address: "Meetupstreet 1, 54321 Meetup City",
     description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      "This is a second, amazing meetup which you definitely should not miss. It will be a lot of fun!",
   },
 ];
 
