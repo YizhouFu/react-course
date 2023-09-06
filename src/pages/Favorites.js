445f9871f141fe5aa2c04465f96f15f75e4c598a
JadFu <yfu5@aceproject.space>
2023-09-06 11:27:06 -0500

    add context and badge
    add context and badge to help tracking the number of favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,23 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+import MeetupItem from "../components/meetups/MeetupItem";
+
 function FavoritesPage() {
-    return <div>Favorites Page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if(favoritesCtx.totalFavorites === 0){
+    content = <p>You have no favorite Meetups Yet</p>
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites}/>;
   }
-  
-  export default FavoritesPage;
+
+  return <section>
+    <h1>My Favorites</h1>
+    {content}
+  </section>;
+}
+
+export default FavoritesPage;
