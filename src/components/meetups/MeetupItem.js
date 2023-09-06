479e87e389051db1781cb57387bae822700cdbcd
JadFu <yfu5@aceproject.space>
2023-09-06 11:17:16 -0500

    add favorite function
    favorite will show on all pages it connects

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,21 +1,39 @@
-import Card from '../ui/Card';
-import classes from "./MeetupItem.module.css"
+import { useContext } from "react";
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MeetupItem(props) {
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.isFavorite(props.id);
+  function toggleFavoriteState() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        image: props.image,
+        address: props.address,
+        discription: props.discription,
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavoriteState}>{itemIsFavorite ? 'Remove From Favorite' : 'Favorite'}</button>
+        </div>
       </Card>
     </li>
   );
