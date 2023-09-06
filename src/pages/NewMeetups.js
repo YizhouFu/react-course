8e82774dff7ae4c4b9ebdc7d44fb59cde398709f
JadFu <yfu5@aceproject.space>
2023-09-06 09:42:32 -0500

    post form
    post and store data on google firebase server

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
-    return <section>
-      <h1>Add new Meetup</h1>
-      <NewMeetupForm></NewMeetupForm>
-    </section>;
+  function handleAddMeetup(meetupData) {
+    fetch(
+      "https://react-course-584bf-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
-  
-  export default NewMeetupsPage;
+
+  return (
+    <section>
+      <h1>Add new Meetup</h1>
+      <NewMeetupForm onAddMeetup={handleAddMeetup} />
+    </section>
+  );
+}
+
+export default NewMeetupsPage;
